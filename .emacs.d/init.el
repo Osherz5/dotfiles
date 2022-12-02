1c6305f7e518c57ebd4f0df701433852130614ee
osherz5@gmail.com <osherz5@gmail.com>
2022-12-02 19:16:58 +0200

    .gitignore for backup files and decrement numbers elisp function

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -270,5 +270,13 @@
 
 (global-set-key (kbd "C-+") 'increment-number-at-point)
 
+(defun decrement-number-at-point ()
+      (interactive)
+      (skip-chars-backward "0-9")
+      (or (looking-at "[0-9]+")
+          (error "No number at point"))
+      (replace-match (number-to-string (1- (string-to-number (match-string 0))))))
+(global-set-key (kbd "C--") 'decrement-number-at-point)
+
 ;; TODO: Add encode hex
 
