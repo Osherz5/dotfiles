a1bfb49ec65a7969881f0e82a77edf6edd6e825e
Osherz5 <osherz5@gmail.com>
2022-12-03 12:48:27 +0200

    Added winget packages, README, and some future tasks

--- bootstrap/bootstrap.bat
+++ bootstrap/bootstrap.bat
@@ -1,3 +1,12 @@
+REM Install packages, this takes a while
+winget import %APPDATA%\.dotfiles\winget\packages.json
+
+REM Should be in PATH
+mkdir C:\tools
+
+REM Create links
+@FOR /F "tokens=* USEBACKQ" %%F IN (`dir /S /B C:\progra~1\Emacs\*runemacs.exe`) DO @(SET EMACS_PATH=%%F)
+mklink C:\tools\runemacs.exe %EMACS_PATH%
 mklink /J %APPDATA%\.emacs.d %APPDATA%\.dotfiles\.emacs.d
 
 pause
