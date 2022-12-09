339cfdc11a5ef6a228d1cd1c6718ab69f11fb951
Osher Jacob <osherz5@gmail.com>
2022-12-09 10:21:54 +0200

    Added linux packages

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -108,7 +108,9 @@
 (load-theme 'solarized-dark t)
 
 
-(use-package transpose-frame)
+(use-package transpose-frame
+  :bind (("s-SPC" . transpose-frame))
+  )
 
 (use-package lsp-mode)
 (use-package lsp-pyright
@@ -256,7 +258,7 @@
 
 
                                         ; Set default browser
-(setq browse-url-generic-program (if (eq system-type 'windows-nt) "c:/Program Files/Google/Chrome/Application/chrome.exe" "/bin/chromium"))
+(setq browse-url-generic-program (if (eq system-type 'windows-nt) "c:/Program Files/Google/Chrome/Application/chrome.exe" "/bin/firefox"))
 (setq browse-url-browser-function 'browse-url-generic)
 
 
@@ -323,31 +325,66 @@
     (require 'exwm-config)
     (exwm-config-default)
 
+    (require 'exwm-randr)
+    (exwm-randr-enable)
+    (start-process-shell-command "xrandr" nil "xrandr --output Virtual-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal")
+
     (require 'exwm-systemtray)
+    (setq exwm-systemtray-height 100)
     (exwm-systemtray-enable)
+
+    ;; These keys should always pass through to Emacs
+    (setq exwm-input-prefix-keys
+          '(?\C-x
+            ?\C-h
+            ?\M-x
+            ?\M-`
+            ?\M-&
+            ?\M-:
+            ?\C-\M-j  ;; Buffer list
+            ?\C-\M-k  ;; Browser list
+            ?\C-\M-n  ;; Next workspace
+            ?\C-\     ;; Ctrl+Space
+            ?\C-\;))
+
+    ;; Ctrl+Q will enable the next key to be sent directly
+    (define-key exwm-mode-map [?\C-q] 'exwm-input-send-next-key)
+
+    ;; Set up global key bindings.  These always work, no matter the input state!
+    ;; Keep in mind that changing this list after EXWM initializes has no effect.
+    (setq exwm-input-global-keys
+          `(
+            ;; Reset to line-mode (C-c C-k switches to char-mode via exwm-input-release-keyboard)
+            ([?\s-r] . exwm-reset)
+
+            ;; Move between windows
+            ([s-left] . windmove-left)
+            ([s-right] . windmove-right)
+            ([s-up] . windmove-up)
+            ([s-down] . windmove-down)
+
+            ;; Launch applications via shell command
+            ([?\s-&] . (lambda (command)
+                         (interactive (list (read-shell-command "$ ")))
+                         (start-process-shell-command command nil command)))
+
+            ;; Switch workspace
+            ([?\s-w] . exwm-workspace-switch)
+            ([?\s-`] . (lambda () (interactive) (exwm-workspace-switch-create 0)))
+
+            ;; 's-N': Switch to certain workspace with Super (Win) plus a number key (0 - 9)
+            ,@(mapcar (lambda (i)
+                        `(,(kbd (format "s-%d" i)) .
+                          (lambda ()
+                            (interactive)
+                            (exwm-workspace-switch-create ,i))))
+                      (number-sequence 0 9))))
     (exwm-enable)
 
-    (when exwm-enabled
-      ;; These keys should always pass through to Emacs
-      (setq exwm-input-prefix-keys
-            '(?\C-x
-              ?\C-h
-              ?\M-x
-              ?\M-`
-              ?\M-&
-              ?\M-:
-              ?\C-\M-j  ;; Buffer list
-              ?\C-\M-k  ;; Browser list
-              ?\C-\M-n  ;; Next workspace
-              ?\C-\     ;; Ctrl+Space
-              ?\C-\;))
-
-      ;; Ctrl+Q will enable the next key to be sent directly
-      (define-key exwm-mode-map [?\C-q] 'exwm-input-send-next-key)
-      
-      )
     )
   )
+
+(setq visible-bell t)
 
 ;; Backup files
 (setq backup-directory-alist
--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -3,3 +3,5 @@ ln -sf ~/.dotfiles/.emacs.d ~/.emacs.d
 
 ln -sf ~/.dotfiles/.xinitrc ~/.xinitrc
 ln -sf ~/.dotfiles/.Xmodmap ~/.Xmodmap
+
+yay -S $(cat ~/.dotfiles/packages.txt)
